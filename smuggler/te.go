811c1a64431f3005d9d86683940d80b9576e955e
AkewakBiru <akawaq1234@gmail.com>
2025-01-26 22:30:22 +0400

    add very basic te.te desync tester, not even sure it works other than on a portswigger lab

--- smuggler/te.go
+++ smuggler/te.go
@@ -5,6 +5,7 @@ import (
 	"smuggler/config"
 	"smuggler/smuggler/h1"
 	"smuggler/smuggler/tests"
+	"time"
 
 	"github.com/rs/zerolog/log"
 )
@@ -17,7 +18,92 @@ func (te *TE) Run() bool {
 	if config.Glob.Concurrent {
 		defer te.Wg.Done()
 	}
-	return te.runTECL()
+	return te.runTETE() || te.runTECL()
+}
+
+func (te *TE) runTETE() bool {
+	log.Info().Str("endpoint", te.URL.String()).Msg("Running TE.TE desync tests...")
+	generator := tests.Generator{}
+	payload := generator.Generate(tests.TE, config.Glob.Test)
+
+	ctr := 0
+	for k, vv := range payload {
+		for _, v := range vv {
+			if err := te.GetCookie(); err != nil {
+				log.Error().Err(err).Msg("")
+				return false
+			}
+			payload := te.NewPl(fmt.Sprintf("%s:%s", k, v))
+			if te._TETE(payload) {
+				ctr++
+				if config.Glob.ExitEarly {
+					log.Info().
+						Str("endpoint", te.URL.String()).
+						Str("status", "success").
+						Msgf("Test stopped on success: PoC payload stored in /result/%s directory", te.URL.Hostname())
+					if config.Glob.Concurrent {
+						te.TestDone <- struct{}{}
+					}
+					return true
+				}
+			}
+			if config.Glob.Concurrent {
+				select {
+				case <-te.Ctx.Done():
+					return false
+				default:
+				}
+			}
+		}
+	}
+	if ctr > 0 {
+		log.Info().
+			Str("endpoint", te.URL.String()).
+			Str("status", "success").
+			Msgf("finished TECL desync tests: PoC payload stored in /result/%s directory", te.URL.Hostname())
+	} else {
+		log.Info().
+			Str("endpoint", te.URL.String()).
+			Str("status", "failure").
+			Msg("finished TECL desync tests: no issues found")
+	}
+	return false
+}
+
+func (te *TE) _TETE(p *h1.Payload) bool {
+	body := "1\r\nG\r\nX\r\n"
+	c := h1.Transport{}
+	pl := te.NewPl(p.HdrPl)
+	pl.Cl = 50
+	pl.Body = body
+
+	req := h1.Request{
+		Url:     te.URL,
+		Payload: pl,
+		Timeout: time.Second * 3,
+	}
+
+	resp, err := c.RoundTrip(&req)
+	if err != nil {
+		return false
+	}
+
+	resp.Body.Close()
+	if resp.StatusCode != 400 { // expect 400 if front-end uses TE
+		return false
+	}
+
+	// send a duplicate TE and large CL header, timeout expected
+	p.HdrPl = fmt.Sprintf("%s\r\n%sx", p.HdrPl, p.HdrPl)
+	p.Cl = 50
+	p.Body = "1\r\nG\r\n0\r\n\r\n"
+	ret, _ := te.H1Test(p)
+	if ret == 1 {
+		log.Info().Msg("This might be a TE.TE desync symptom")
+		te.GenReport(p)
+		return true
+	}
+	return false
 }
 
 func (te *TE) runTECL() bool {
